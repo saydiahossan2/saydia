230a0326043bd9b8090aa7dec9a5a7a4ff53f8f8
saydiahossan2 <54040066+saydiahossan2@users.noreply.github.com>
2019-08-12 12:17:43 -0700

    Add files via upload

---
+++ temp saydia spyder 2019.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 12 11:25:45 2019
+
+@author: Saydia
+"""
+
+print("hi there") #welcome
+print ("2001")
+print(56)
+
+
+#print 78
+
+
+
+
+print(5*2)
+print(5/2)
+print(5-2)
+print(5**2)
+print(12+98)
+
+
+def add(a,b):
+  add = a+b
+  print(add)
+
+add(2,3)
+
+add(10985.345,9802.472)
+
+add(9067.987,6752.891)
+
+
+add(678.89,908.34,765.23)
+
+
+def adds(a,b,c):
+     adds = a+b+c
+     print(adds)
+     
+adds(9,7,4)
+
+adds(78,65,38)
+
+
+
+#saydia's code
+def add(a,b):
+    add=a+b
+    print("the sum is", add)
+
+add(908,675)
+
+def add(a,c)
